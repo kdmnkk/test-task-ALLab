b0852cefc27adfe270b033432abe80fd30045ea6
Anastasia <kdmnk.an@gmail.com>
2022-11-09 16:52:13 +0200

    feat: create job api service and job interface

---
+++ src/app/core/interfaces/job.interface.ts
@@ -0,0 +1,19 @@
+export interface JobInterface {
+  id: string,
+  name: string,
+  email: string,
+  phone:string,
+  title: string,
+  salary: string,
+  address: string,
+  benefits: string[],
+  location: {
+    lat: number,
+    long: number
+  },
+  pictures: string[],
+  "createdAt": string,
+  "updatedAt": string,
+  "description": string,
+  "employment_type": string[]
+}
