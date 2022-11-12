f871b0392c06d101ff980a0c3b9a2ba5032138d9
Anastasia <kdmnk.an@gmail.com>
2022-11-12 15:03:51 +0200

    feat: finding location by Geocoder Service

--- src/app/components/job-card/job-card.component.ts
+++ src/app/components/job-card/job-card.component.ts
@@ -1,15 +1,16 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { MapGeocoder } from '@angular/google-maps';
+import { delay } from 'rxjs';
 import { JobInterface } from 'src/app/core/interfaces/job.interface';
 
 @Component({
   selector: 'app-job-card',
   templateUrl: './job-card.component.html',
-  styleUrls: ['./job-card.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class JobCardComponent implements OnInit {
   @Input() job?: JobInterface;
+  @Input() index = 0;
 
   location = '-';
 
@@ -19,13 +20,13 @@ export class JobCardComponent implements OnInit {
     if (this.job) {
       this.geocoder.geocode({
         location: {
-          lat: this.job?.location.lat ? this.job?.location.lat : 40.714224,
-          lng: this.job?.location.long ? this.job?.location.long : -73.961452,
+          lat: +this.job?.location.lat,
+          lng: +this.job?.location.long,
         },
-      }).subscribe(({ results }) => {
-        this.location = results.length ? results[0].formatted_address : '-';
+      }).pipe(delay(this.index * 1000)).subscribe(({ results }) => {
+        this.location = results.length ? results[0].formatted_address : 'New York';
+        this.cdr.detectChanges();
       });
-      this.cdr.detectChanges();
     }
   }
 }
